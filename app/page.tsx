2dbb2bf4081914577850ca3ebecdf87b7f95f606
juan-phzy <jp.hernandez.yz@gmail.com>
2023-12-02 16:05:41 -0500

    installed react-icons dependency

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@
 import Navbar from "@/app/components/Navbar";
 import Image from "next/image";
 import homeImage from "@/public/images/homeImage.jpg";
+import { Button } from "./components/ui/button";
+
 //-------------------------------------------------------
 /*
 	This is the main landing page that is rendered when
@@ -18,7 +20,7 @@ export default function Page() {
 			------------------------------------------------------------------------------------------------------
 			Main container which fills the full viewport. The Navbar and Body go inside stacked as a flex column - juan
 			----------------------------------------------------------------------------------------------------*/}
-			<main className="Site-Container w-screen h-screen flex flex-col">
+			<main className="Site-Container w-screen h-screen flex flex-col min-w-[1111px]">
 				{/* 
 				-----------------------------------------------------------------------------------------------
 				Container for the Navbar. The Navbar component goes inside. Fills width and fits content height - juan
@@ -30,18 +32,20 @@ export default function Page() {
 				-----------------------------------------------------------------------------------------------------
 				Container for the body. The body content goes inside. Content is centered and 50px padding all around - juan
 				----------------------------------------------------------------------------------------------------*/}
-				<section className="Main-Content w-full h-full flex flex-row justify-center items-center bg-neutral-700 p-[50px]">
+				<section className="w-full h-full flex flex-row justify-center items-center bg-neutral-700 p-[50px]">
 					{/*
 					-----------------------------------------------------------------------------------------------------------
-					Background container for body content. Body content includes two sections stacked side by side as flex rows.
-						Section 1 of body content will be an image, Section 2 will be text - juan
+					Background container for body content. 
+						- Body content includes 2 sections stacked side by side as flex rows.
+						- Section 1 of body content will be an image, Section 2 will be text
+						- 2rem(32px) gap between s	ections - juan
 					-----------------------------------------------------------------------------------------------------------*/}
-					<section className="Content-Background w-full h-full flex flex-row justify-center items-center gap-x-8 bg-neutral-400 p-x-24 ">
+					<section className="w-full h-full flex flex-row justify-center items-center gap-x-8 bg-neutral-400 p-[75px] ">
 						{/*
 						-------------------------------------------------------------
 						Section 1: Image container. Image component goes inside - juan
 						------------------------------------------------------------*/}
-						<section className="Image-Container w-2/5 h-fit border-8 border-black flex justify-center items-center text-white text-xl">
+						<section className="max-w-[600px] w-4/9 h-full flex justify-center items-center text-white text-xl">
 							<Image src={homeImage} alt="Home image" />
 						</section>
 						{/*
@@ -49,12 +53,12 @@ export default function Page() {
 						Section 2: Text container. Two sub-sections go inside stacked as flex columns:
 							Sub-section-1: Welcome text, Sub-section-2: User Account Buttons  - juan
 						------------------------------------------------------------*/}
-						<section className="w-[450px] h-full border-8 border-black flex flex-col justify-center gap-y-8 items-center text-white text-5xl bg-neutral">
+						<section className="max-w-[600px] w-4/9 h-full flex flex-col justify-center gap-y-8 items-center text-white text-5xl bg-neutral">
 							<div>A new way to search for the sounds you love</div>
 							<div className="w-full flex flex-row gap-x-8 text-xl">
 								{/*Subsitute bottom two divs with button components with user account functionality*/}
-								<div>Log in.</div>
-								<div>Create Account.</div>
+								<Button variant="ghost">Log In.</Button>
+								<Button variant="ghost">Create Account.</Button>
 							</div>
 						</section>
 					</section>
